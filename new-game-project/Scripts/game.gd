bef2d328700d9f0d4348300cd9c0c70a3c3d6725
1bucket <penrade43@gmail.com>
2025-02-22 16:03:20 -0500

    added wind timer

--- new-game-project/Scripts/game.gd
+++ new-game-project/Scripts/game.gd
@@ -5,8 +5,12 @@ var player
 var time_elapsed = 0 # in seconds
 
 var wind_rng = RandomNumberGenerator.new()
-var wind_chance = 0.1
-var wind_duration = 0
+var wind_chance : float = 0.1
+var wind_interval : float = 5 # in seconds
+var wind_start : float = 0
+var wind_variation : float = 1 # in velocity units
+var wind_default_duration : float = 10 # in seconds
+var is_windy : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,8 +21,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_elapsed += delta
+	
+	# will be retrieved from gyroscope input
+	var gyro_x_accel = 0 # stub
 	# every 5 seconds, wind_chance chance of applying random wind acceleration
-	if (time_elapsed % 5) == 0 && wind_rng.randf_range(0, 1) < wind_chance:
-		# apply wind, stub 
-		wind_rng.randf_range(-1, 1)
+	var new_x_accel = gyro_x_accel
+	
+	if !is_windy && (time_elapsed % wind_interval) == 0 && wind_rng.randf_range(0, 1) < wind_chance:
+		is_windy = true
+		wind_start = time_elapsed
+	else:
+		is_windy = time_elapsed - wind_start < wind_default_duration
+		new_x_accel += wind_rng.randf_range(-wind_variation, wind_variation)
+	
+	player.accel.x = new_x_accel
 	pass
--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -1,9 +1,10 @@
 extends Node
 
 @export var animatedSprite: AnimatedSprite2D
-var ang = 0
-var ang_velocity = 0.0 # measured in degrees per second
-var ang_accel = 0.0 # measured in degrees per second per second
+var x = 0.0
+var y = 0.0
+var velocity = Vector2(0, 0)
+var accel = Vector2(0, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,15 +13,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# apply physics vectors
-	ang += ang_velocity * delta
-	ang_velocity += ang_accel * delta
-	if(ang_velocity < 0):
-		animatedSprite.play("tilt_left")
-	elif(ang_velocity > 0):
-		animatedSprite.play("tilt_right")
-	else:
-		animatedSprite.play("idle")
+	x += velocity.x
+	y += velocity.y
+	velocity.x += accel.x
+	velocity.y += accel.y
 	pass
-
-func set_accel(change_accel : float) -> void:
-	ang_accel += change_accel
