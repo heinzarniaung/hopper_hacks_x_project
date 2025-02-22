feb92ef1eb910de2aae076342cacda14fcb608f7
NotLawrence0101 <94724659+NotLawrence0101@users.noreply.github.com>
2025-02-22 17:12:35 -0500

    Player Controller

--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -1,26 +1,31 @@
-extends Node
+extends CharacterBody2D  # Changed from Node to CharacterBody2D
 
 @export var animatedSprite: AnimatedSprite2D
+var speed = 400
+var gravity = 400
 var ang = 0
-var ang_velocity = 0.0 # measured in degrees per second
-var ang_accel = 0.0 # measured in degrees per second per second
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var ang_velocity = 0.0  # Measured in degrees per second
+var ang_accel = 0.0  # Measured in degrees per second per second
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# apply physics vectors
+	# Apply rotation physics
 	ang += ang_velocity * delta
 	ang_velocity += ang_accel * delta
-	if(ang_velocity < 0):
+
+	# Get accelerometer input for movement
+	velocity.x = Input.get_accelerometer().normalized().x * speed
+
+	# Play animation based on angular velocity
+	if ang_velocity < 0:
 		animatedSprite.play("tilt_left")
-	elif(ang_velocity > 0):
+	elif ang_velocity > 0:
 		animatedSprite.play("tilt_right")
 	else:
 		animatedSprite.play("idle")
-	pass
 
-func set_accel(change_accel : float) -> void:
+func set_accel(change_accel: float) -> void:
 	ang_accel += change_accel
+
+func _physics_process(delta):
+	velocity.y += gravity * delta 
+	move_and_slide()  
