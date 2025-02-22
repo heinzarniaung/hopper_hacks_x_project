b019d4bbd85051a6d730e3e8527cc9164a52e5d4
Brian Chau <brianchau0729@gmail.com>
2025-02-22 14:29:11 -0500

    Mad end screne

---
+++ new-game-project/Scripts/global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var highScore = 0
+#Use this for unlocks if time permits
+var doubloons = 0
+
+func _ready() -> void:
+	#Add save load if there is time
+	pass # Replace with function body.
