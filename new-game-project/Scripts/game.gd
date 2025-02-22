ff01770022fbc79e5761b6b5c2cfa770f7ac539d
1bucket <penrade43@gmail.com>
2025-02-22 18:24:17 -0500

    fixed player movement + wind influence + gravity

--- new-game-project/Scripts/game.gd
+++ new-game-project/Scripts/game.gd
@@ -10,10 +10,14 @@ var wind_rng = RandomNumberGenerator.new()
 var wind_chance = 0.1
 var wind_strength = 0
 var wind_duration = 10
+var wind_variation = 100
+
+var max_speed = 400
+var gravity = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#player = get_node("Player")
+	player = get_node("Player")
 	pass # Replace with function body.
 
 
@@ -24,14 +28,17 @@ func _process(delta: float) -> void:
 	# every 5 seconds, wind_chance chance of applying random wind acceleration
 	if time_elapsed > time_till_next:
 		time_elapsed = 0
-		wind_strength = wind_rng.randf_range(-1,1)
+		wind_strength = wind_rng.randf_range(-wind_variation,wind_variation)
 		time_till_next = (wind_rng.randf_range(0,1) * 5) + 10
-		if(wind_strength < 0):
+		if(wind_strength > 0):
 			activateWindLeft()
-		elif(wind_strength > 0):
+		elif(wind_strength < 0):
 			activateWindRight()
 	if time_elapsed > wind_duration:
 		turnOffWind()
+	player.accel.x = wind_strength + Input.get_accelerometer().normalized().x * max_speed
+	player.accel.y = gravity
+	
 
 func activateWindLeft():
 	windLeft.visible = true
@@ -44,6 +51,7 @@ func activateWindRight():
 	pass
 	
 func turnOffWind():
+	wind_strength = 0
 	windLeft.visible = false
 	windRight.visible = false
 	windLeft.turnOff()
--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -1,31 +1,19 @@
 extends CharacterBody2D  # Changed from Node to CharacterBody2D
 
 @export var animatedSprite: AnimatedSprite2D
-var speed = 400
-var gravity = 400
-var ang = 0
-var ang_velocity = 0.0  # Measured in degrees per second
-var ang_accel = 0.0  # Measured in degrees per second per second
+var accel = Vector2()
 
 func _process(delta: float) -> void:
-	# Apply rotation physics
-	ang += ang_velocity * delta
-	ang_velocity += ang_accel * delta
 
 	# Get accelerometer input for movement
-	velocity.x = Input.get_accelerometer().normalized().x * speed
-
+	velocity.x += accel.x
+	velocity.y += accel.y
+	# apply physics
+	move_and_slide()
 	# Play animation based on angular velocity
-	if ang_velocity < 0:
+	if velocity.x < 0:
 		animatedSprite.play("tilt_left")
-	elif ang_velocity > 0:
+	elif velocity.x > 0:
 		animatedSprite.play("tilt_right")
 	else:
 		animatedSprite.play("idle")
-
-func set_accel(change_accel: float) -> void:
-	ang_accel += change_accel
-
-func _physics_process(delta):
-	velocity.y += gravity * delta 
-	move_and_slide()  
