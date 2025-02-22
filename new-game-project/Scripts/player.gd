92e250a289083da9e3b11b4e3b7f28f4c2dfe5b4
1bucket <penrade43@gmail.com>
2025-02-22 17:14:57 -0500

    added player movement

--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -1,20 +1,25 @@
-extends Node
+extends CharacterBody2D
 
 @export var animatedSprite: AnimatedSprite2D
-var x = 0.0
-var y = 0.0
-var velocity = Vector2(0, 0)
 var accel = Vector2(0, 0)
+var velX = 0
+var velY = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#velocity.x = 50
+	#velocity.y = 50
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	print("x velocity " + str(velX))
+	#print("x accel " + str(accel.x))
 	# apply physics vectors
-	x += velocity.x
-	y += velocity.y
-	velocity.x += accel.x
-	velocity.y += accel.y
+	velX += accel.x
+	velY += accel.y
+	velocity.x = velX
+	velocity.y = velY
+	move_and_slide()
 	pass
