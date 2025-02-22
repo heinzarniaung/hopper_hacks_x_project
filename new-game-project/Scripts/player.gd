37d715d1815c79423231bc22ea23dff69ef4ad2f
Brian Chau <brianchau0729@gmail.com>
2025-02-22 15:29:00 -0500

    Added code to change sprite based on ang

--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -1,9 +1,9 @@
 extends Node
 
+@export var animatedSprite: AnimatedSprite2D
 var ang = 0
 var ang_velocity = 0.0 # measured in degrees per second
 var ang_accel = 0.0 # measured in degrees per second per second
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,8 +14,13 @@ func _process(delta: float) -> void:
 	# apply physics vectors
 	ang += ang_velocity * delta
 	ang_velocity += ang_accel * delta
-	
+	if(ang_velocity < 0):
+		animatedSprite.play("tilt_left")
+	elif(ang_velocity > 0):
+		animatedSprite.play("tilt_right")
+	else:
+		animatedSprite.play("idle")
 	pass
 
-func set_accel(new_accel : float) -> void:
-	ang_accel = new_accel
+func set_accel(change_accel : float) -> void:
+	ang_accel += change_accel
